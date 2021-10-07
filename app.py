949e38285ed52531a4551f62e87fa204acf2279e
bioalejo <bioalejandrogomez@hotmail.com>
2021-10-06 23:42:52 -0500

    Tabla productos 1.0

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/productos')
+def productos():
+    return render_template('modules/products.html')
 
 
 if __name__ == '__main__':
