49d7bb72a756e6e8915480e0353f7a4f30dcedcb
leslysharyn <leslysharyncj@gmail.com>
2021-10-07 21:04:25 -0500

    Mi primer cambio :)

--- app.py
+++ app.py
@@ -3,10 +3,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-    
+
+@app.route('/productos')
+def productos():
+    return render_template('modules/products.html')
+
+@app.route('/login')
+def login():
+    return render_template('modules/login.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5504)
