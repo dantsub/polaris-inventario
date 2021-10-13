bf5326d5851213df2f67be82b443d90fd740536b
leslysharyn <leslysharyncj@gmail.com>
2021-10-13 13:29:26 -0500

    Validación .py modal editar de user

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 import utils
 import os
 
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
@@ -53,7 +54,6 @@ def login():
         error = False
         errorusuario = ""
         errorclave=""
-        
         if not utils.isUsernameValid(usuario):
             errorusuario = "El usuario debe ser alfanumérico y/o contener -_."
             error= True
@@ -73,9 +73,102 @@ def login():
 def providers():
     return render_template('modules/providers.html')
 
+@app.route('/crearprovider', methods=["GET", "POST"])
+def crearproveedor():
+
+    data1 = "" #error Idproveedor #id
+    data2 = "" # error nombre #nombre
+    data3 = "" # error correo #correo
+    data4 = "" # error teléfono #telefono
+    data5 = "" # error direccion #direccion
+    data6 = "" # error pais #pais
+    error = False
+    id = ""
+    if request.method == 'POST':
+        id= request.form.get('id')
+        nombre= request.form.get('nombre')
+        correo= request.form.get('correo')
+        telefono= request.form.get('telefono')
+        direccion= request.form.get('direccion')
+        pais= request.form.get('pais')
+
+        if id == "":
+            data1 = "El campo Id del Proveedor no puede estar vacio"
+            error = True
+        if nombre == "":
+            data2 = "El campo Nombre no puede estar vacio"
+            error = True
+        if correo == "":
+            data3 = "El campo Correo no puede estar vacio"
+            error = True
+        if telefono == "":
+            data4 = "El campo Telefono no puede estar vacio"
+            error = True
+        if direccion == "":
+            data5 = "El campo Dirección no puede estar vacio"
+            error = True
+        if pais == "":
+            data6 = "El campo clave no puede estar vacio"
+            error = True
+    return render_template('modules/users.html', data1=data1, data2=data2, data3=data3, data4=data4, data5=data5, data6=data6, error=error, id=id, nombre=nombre, correo=correo, telefono=telefono, direccion=direccion,  pais=pais)
+
+
+@app.route('/crearuser', methods=["GET", "POST"])
+def crearproveedorin():
+    return render_template('modules/users.html')
+
 
 @app.route('/users', methods=["GET", "POST"])
 def users():
+    return render_template('modules/users.html')
+
+@app.route('/crearuser', methods=["GET", "POST"])
+def crearusuario():
+
+    data1 = "" #error usuario #usuario
+    data2 = "" # error nombres #nombres
+    data3 = "" # error apellidos #apellidos
+    data4 = "" # error documento #cedula
+    data5 = "" # error correo #correo
+    data6 = "" # error rol #rol_crear
+    data7 = "" # error clave #passw
+    error = False
+    usuario = ""
+    if request.method == 'POST':
+        usuario= request.form.get('usuario')
+        nombre= request.form.get('nombres')
+        apellido= request.form.get('apellidos')
+        documento= request.form.get('cedula')
+        correo= request.form.get('correo')
+        rol= request.form.get('rol_crear')
+        clave= request.form.get('passw')
+
+        if usuario == "":
+            data1 = "El campo usuario no puede estar vacio"
+            error = True
+        if nombre == "":
+            data2 = "El campo Nombres no puede estar vacio"
+            error = True
+        if apellido == "":
+            data3 = "El campo Apellidos no puede estar vacio"
+            error = True
+        if documento == "":
+            data4 = "El campo Documentos no puede estar vacio"
+            error = True
+        if correo == "":
+            data5 = "El campo Correo no puede estar vacio"
+            error = True
+        if rol == None:
+            data6 = "Seleccione un rol"
+            error = True
+        if clave == "":
+            data7 = "El campo clave no puede estar vacio"
+            error = True
+    return render_template('modules/users.html', data1=data1, data2=data2, data3=data3, data4=data4, data5=data5, data6=data6, data7=data7, error=error, nombre=nombre, usuario=usuario, apellido=apellido, clave=clave, documento=documento, correo=correo, rol=rol)
+
+
+@app.route('/crearuser', methods=["GET", "POST"])
+def crearusuariosin():
     return render_template('modules/users.html')
 
 
