e0c47266a9c58a182da4b8a8e167ee1f70e7395a
bioalejo <bioalejandrogomez@hotmail.com>
2021-10-27 14:28:38 -0500

    Fox listar disponibles y minimos productos

--- productos.py
+++ productos.py
@@ -94,7 +94,7 @@ def listardisponibles():
     conexion = sqlite3.connect("Polaris")
 
     cursor = conexion.cursor()
-    cursor.execute("SELECT p.idProducto, p.nombre, p.descripcion, p.cantminima, p.cantdisponible, c.nombre, ( SELECT ROUND(AVG(valor), 1) AS avg_amount FROM calificacion WHERE p.idProducto=idProducto GROUP BY idProducto) AS promedio FROM producto p JOIN proveedores c WHERE p.idProveedor=c.idProveedor and p.cantdisponible>0")
+    cursor.execute("SELECT p.idProducto, p.nombre, p.descripcion, p.cantminima, p.cantdisponible, c.nombre, p.idProveedor, (SELECT ROUND(AVG(valor),1) AS avg_amount FROM calificacion WHERE p.idProducto=idProducto GROUP BY idProducto) AS promedio FROM producto p JOIN proveedores c WHERE p.idProveedor=c.idProveedor and p.cantdisponible>0")
     filas = cursor.fetchall()
     conexion.close()
     print(filas)
@@ -105,7 +105,7 @@ def productosdebajominimo():
     conexion = sqlite3.connect("Polaris")
 
     cursor = conexion.cursor()
-    cursor.execute("SELECT p.idProducto, p.nombre, p.descripcion, p.cantminima, p.cantdisponible, c.nombre, ( SELECT ROUND(AVG(valor), 1) AS avg_amount FROM calificacion WHERE p.idProducto=idProducto GROUP BY idProducto) AS promedio FROM producto p JOIN proveedores c WHERE p.idProveedor=c.idProveedor and p.cantdisponible<p.cantminima")
+    cursor.execute("SELECT p.idProducto, p.nombre, p.descripcion, p.cantminima, p.cantdisponible, c.nombre, p.idProveedor, (SELECT ROUND(AVG(valor),1) AS avg_amount FROM calificacion WHERE p.idProducto=idProducto GROUP BY idProducto) AS promedio FROM producto p JOIN proveedores c WHERE p.idProveedor=c.idProveedor and p.cantdisponible<p.cantminima")
     filas = cursor.fetchall()
     conexion.close()
     print(filas)
